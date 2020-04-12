561a6960e67604b8d7d0006185432496993fec39
jack阳 <you@example.com>
2020-04-12 16:55:04 +0800

    加入显示天气信息的功能。

---
+++ app/src/main/java/com/funnyweather/android/logic/network/PlaceService.kt
@@ -0,0 +1,12 @@
+package com.funnyweather.android.logic.network
+
+import com.funnyweather.android.FunnyWeatherApplication
+import com.funnyweather.android.logic.model.PlaceResponse
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface PlaceService {
+    @GET("v2/place?token=${FunnyWeatherApplication.TOKEN}&lang=zh_CN")
+    fun searchPlaces(@Query("query") query: String): Call<PlaceResponse>
+}
